7a4b2b4136d06fcd30b3f30ade82b40df16720a5
glitched-sudhanshu <r02.sudhanshu@gmail.com>
2024-12-06 03:57:56 +0530

    [feat][wip] initial setup

---
+++ app/src/main/java/com/example/entertainmentstudio/ui/savedNews/SavedNewsFragment.java
@@ -0,0 +1,37 @@
+package com.example.entertainmentstudio.ui.savedNews;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.entertainmentstudio.databinding.FragmentGalleryBinding;
+
+public class SavedNewsFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SavedNewsViewModel galleryViewModel =
+                new ViewModelProvider(this).get(SavedNewsViewModel.class);
+
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textGallery;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
